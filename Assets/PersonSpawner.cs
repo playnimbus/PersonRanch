16d460d45141a6574cca6dfcc6b66ef9b09877d0
Michael Flood <Nick@Nicks-Mac-mini.local>
2015-10-24 06:34:28 -0400

    adding person spawner?

---
+++ Assets/PersonSpawner.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PersonSpawner : MonoBehaviour {
+
+	public Terrain terrain;
+	private Vector3 terrainBounds;
+	// Use this for initialization
+
+	void Awake(){
+		terrainBounds = terrain.terrainData.size;
+		
+		for(int i = 0; i < 50;i++){
+			GameObject instance = Instantiate(Resources.Load("Person", typeof(GameObject))) as GameObject;
+			instance.transform.position = new Vector3(Random.Range(1,terrainBounds.x -1),1,Random.Range(1,terrainBounds.z -1));
+		}
+	}
+}
