ea9d0eabcdb27132279c8235884d58f54d97f016
Adam <adam@playnimbus.com>
2015-10-24 14:32:02 -0400

    Car sounds

--- Assets/Car.cs
+++ Assets/Car.cs
@@ -1,6 +1,8 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[RequireComponent(typeof(AudioSource))]
+[RequireComponent(typeof(Rigidbody))]
 public class Car : MonoBehaviour 
 {
     public static Car Instance
@@ -16,19 +18,29 @@ public class Car : MonoBehaviour
     public float maxReverseSpeed;
     public float turnRate;
 
+    public AudioClip initialRev;
+    public AudioClip loopRev;
+    public AudioClip stopRev;
+
     private float targetSpeed = 0;
     private float forward;
     private Rigidbody rigidbody;
+    private AudioSource audioSource;
 
     void Awake()
     {
         instance = this;
         rigidbody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+        audioSource.clip = initialRev;
+        audioSource.loop = false;
     }
 
 	// Update is called once per frame
 	void Update () 
     {
+        bool wasStill = Mathf.Abs(targetSpeed) < 0.1f;
+
         if (Input.GetKey(KeyCode.W))
         {
             targetSpeed += forwardAcceleration * Time.deltaTime;
@@ -54,12 +66,48 @@ public class Car : MonoBehaviour
         if (Input.GetKey(KeyCode.D))
         {
             float factor = targetSpeed > 0 ? targetSpeed / maxForwardSpeed : -targetSpeed / maxReverseSpeed;
-            factor = Mathf.Max(factor, 0.5f);
+            factor = Mathf.Max(factor*2f, 0.5f);
             forward -= turnRate * factor * Time.deltaTime;
         }
 
+        bool moving = Mathf.Abs(targetSpeed) > 0.1f;
+
+        // Started moving
+        if (wasStill && moving)
+        {
+            if(audioSource.clip != initialRev || !audioSource.isPlaying)
+            {
+                audioSource.Stop();
+                audioSource.clip = initialRev;
+                audioSource.Play();
+            }
+        }
+
+        // Stopped moving
+        if (!wasStill && !moving)
+        {
+            if (audioSource.clip != stopRev || !audioSource.isPlaying)
+            {
+                audioSource.Stop();
+                audioSource.clip = stopRev;
+                audioSource.Play();
+            }
+        }
+
         transform.rotation = Quaternion.AngleAxis(forward * Mathf.Rad2Deg * -1, Vector3.up);
+
+        UpdateAudio();
 	}
+
+    void UpdateAudio()
+    {
+        bool moving = Mathf.Abs(targetSpeed) > 0.1f;
+        if (!audioSource.isPlaying && moving)
+        {
+            audioSource.clip = loopRev;
+            audioSource.Play();
+        }
+    }
 
     void FixedUpdate()
     {
