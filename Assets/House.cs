8939d1c0d0b996be8f8d9b084ea3385a572a6a6c
Adam <adam@playnimbus.com>
2015-10-24 13:14:49 -0400

    Bug fixing, text

--- Assets/House.cs
+++ Assets/House.cs
@@ -1,25 +1,40 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections.Generic;
 
 public class House : MonoBehaviour 
 {
-    List<Person> people = new List<Person>();
+    public int familySize;
+    public Text counter;
+
+    private List<Person> people = new List<Person>();
+    private HashSet<Person> avoid = new HashSet<Person>();
+
+    void Start()
+    {
+        UpdateText();
+    }
 
     void OnCollisionEnter(Collision collision)
     {
         Person p = collision.gameObject.GetComponent<Person>();
         if (p != null)
         {
-            if (p.stuckToCar)
+            if (isFull) return;
+
+            if (p.stuckToCar && !avoid.Contains(p))
             {
                 if (p.hermit)
                 {
                     foreach (Person person in people)
                     {
                         person.gameObject.SetActive(true);
-                        p.transform.position = (person.transform.position - transform.position).normalized * 4 + transform.position;
+                        p.transform.position = (person.transform.position - transform.position).normalized + transform.position;
+                        p.stuckToCar = false;
                         person.GetComponent<Rigidbody>().AddForce((person.transform.position - transform.position).normalized * person.GetComponent<Rigidbody>().mass, ForceMode.Impulse);
+                        avoid.Add(person);
                     }
+                    Invoke("ClearAvoid", 5f);
                     people.Clear();
 
                     p.DetachFromCar();
@@ -28,11 +43,34 @@ public class House : MonoBehaviour
                 }
                 else
                 {
+                    
                     p.DetachFromCar();
                     p.gameObject.SetActive(false);
                     people.Add(p);
                 }
             }
+
+            UpdateText();
         }
+    }
+
+    void Update()
+    {
+        counter.transform.rotation = Quaternion.LookRotation(transform.position - Camera.main.transform.position);
+    }
+
+    private void UpdateText()
+    {
+        counter.text = people.Count + "/" + familySize;
+    }
+
+    private bool isFull
+    {
+        get { return people.Count == familySize; }
+    }
+
+    void ClearAvoid()
+    {
+        avoid.Clear();
     }
 }
