70a867a67d7016736e63e7150a74039967ba0167
Nick Mudry <nick@mudry.me>
2015-10-24 16:42:00 -0400

    You can drive the main menu! :D

---
+++ Assets/rotationoflights.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class rotationoflights : MonoBehaviour {
+    public float speed;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        speed++;
+        gameObject.transform.rotation = new Quaternion(gameObject.transform.rotation.x, gameObject.transform.rotation.y + speed, gameObject.transform.rotation.z, gameObject.transform.rotation.w);
+	}
+}
